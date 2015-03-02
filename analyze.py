6fa2f62b4979e8fd9a421c5221efb34e109c7b56
Leon Helwerda <l.s.helwerda@umail.leidenuniv.nl>
2015-03-02 16:43:24 +0100

    Added message display with word scores coloring
    In analyze.py, display the messages on one line by stripping newlines.
    Also show the words that affected the score with a respective color in
    front of it.
    
    Best way to run this is:
    $ python analyze.py < commit_comments.json | less -SR
    This makes less not wrap lines and show the colors correctly.
    
    Later on with real classifiers we can disable/remove this display since
    it will probably not be needed then.

--- analyze.py
+++ analyze.py
@@ -13,20 +13,26 @@ def main(argv):
         for line in file:
             words[line.rstrip()] = -1
 
+    colors = {1: '\033[92m', -1: '\033[91m'}
+    END_COLOR = '\033[0m'
+
     # Perform the sentiment analysis
     for jsonObject in sys.stdin:
         data = json.loads(jsonObject)
-        message = data["body"]
+        # normalize newlines
+        message = data["body"].replace('\r\n','\n')
         score = 0
         found = 0
+        disp = ""
         for w in message.split():
             # Only keep alphanumeric and some punctuation characters
             w = re.sub(r'[^\-\'+\w]', '', w).lower()
             if w in words:
                 score += words[w]
                 found += 1
+                disp += colors[words[w]] + w + END_COLOR + " "
 
-        print("{}\t{:.2f}".format(data["id"], score / float(found) if found != 0 else 0.0))
+        print("{}\t{:.2f}\t{}{}".format(data["id"], score / float(found) if found != 0 else 0.0, disp, message.replace('\n',' ')))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
