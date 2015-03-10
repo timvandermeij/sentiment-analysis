337c22dcd3bdf80ce16c75ee55bc8f1224c89ba0
Leon Helwerda <leon.helwerda@planet.nl>
2015-03-10 15:54:31 +0100

    Regression predictions using autolabeled train set
    - Made analyze.py more modular by adding an Analyzer class
    - classify.py now creates an artificial training set by running the
      first 1000 samples through the analyzer and tracking their scorings.
      Then a pipeline of TFIDF and RandomForestRegresson fits the data and
      creates predictions for 2000 samples (to keep it slightly fast...)
      Then we can see how this regression performs on the rest of the data.

--- analyze.py
+++ analyze.py
@@ -2,28 +2,22 @@ import sys
 import re
 import json
 
-def main(argv):
-    group = argv[0] if len(argv) > 0 else "id"
-    display = group == "id"
-
-    # Load the positive and negative words
-    words = {}
-    with open("words/positive.txt") as file:
-        for line in file:
-            words[line.rstrip()] = 1
-
-    with open("words/negative.txt") as file:
-        for line in file:
-            words[line.rstrip()] = -1
-
-    colors = {1: '\033[92m', -1: '\033[91m'}
+class Analyzer(object):
     END_COLOR = '\033[0m'
 
-    # Perform the sentiment analysis
-    for jsonObject in sys.stdin:
-        data = json.loads(jsonObject)
-        # normalize newlines
-        message = data["body"].replace('\r\n','\n')
+    def __init__(self):
+        # Load the positive and negative words
+        self.words = {}
+        self.colors = {1: '\033[92m', -1: '\033[91m'}
+        with open("words/positive.txt") as file:
+            for line in file:
+                self.words[line.rstrip()] = 1
+
+        with open("words/negative.txt") as file:
+            for line in file:
+                self.words[line.rstrip()] = -1
+
+    def analyze(self, message, display=False):
         score = 0
         found = 0
         disp = ""
@@ -31,11 +25,25 @@ def main(argv):
         # Keep emoticons together but beware of edge cases that should be split
         parts = filter(lambda x: x != '' and x is not None, re.split(r'"|(?:(?<=[a-z])[;\.])?\s+|(?:(?<=[a-z])[;\.])?$|(?!.[/(]\S)([^;\.\-\"\'+\w\s][^+\w\s]*(?:[-a-z]\b)?)|(?!.[/(]\S)((?:\b[a-z])?[^+\w\s]*[^;\.\-\"\'+\w\s])', message.lower()))
         for w in parts:
-            if w in words:
-                score += words[w]
+            if w in self.words:
+                score += self.words[w]
                 found += 1
                 if display:
-                    disp += colors[words[w]] + w + END_COLOR + " "
+                    disp += self.colors[self.words[w]] + w + self.END_COLOR + " "
+
+        return (score, found, disp)
+
+def main(argv):
+    group = argv[0] if len(argv) > 0 else "id"
+    display = group == "id"
+
+    # Perform the sentiment analysis
+    for jsonObject in sys.stdin:
+        data = json.loads(jsonObject)
+        # normalize newlines
+        message = data["body"].replace('\r\n','\n')
+
+        (score, found, disp) = analyze(message, display)
 
         print("{}{:.2f}{}".format(str(data[group]) + "\t" if group != "score" else "", score / float(found) if found != 0 else 0.0, "\t{}{}".format(disp, message.replace('\n',' ')) if display else ""))
 
--- classify.py
+++ classify.py
@@ -1,26 +1,47 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
 import numpy as np
 import json
 import sys
+import time
+from analyze import Analyzer # for some train data labelling
 
-def read_json(file):
+def read_json(file, max=None):
     i = 0
     for jsonObject in file:
         data = json.loads(jsonObject)
         message = data["body"].replace('\r\n', '\n')
         yield message
         i = i + 1
-        if i % 1000 == 0:
-            print(i)
-            return
+        if max is not None and i >= max:
+            break
 
 def main(argv):
-    cv = CountVectorizer(input='content')
-    train_counts = cv.fit_transform(read_json(sys.stdin))
-    #print(train_counts)
-    tt = TfidfTransformer()
-    tfidf = tt.fit_transform(train_counts)
-    #print(tfidf)
+    train_data = []
+    train_labels = []
+    analyzer = Analyzer()
+    for message in read_json(sys.stdin):
+        (score, found, _) = analyzer.analyze(message)
+        label = score / float(found) if found > 0 else 0.0
+        train_data.append(message)
+        train_labels.append(label)
+
+        if len(train_data) >= 1000:
+            break
+
+    regressor = Pipeline([('tfidf', TfidfVectorizer(input='content')),
+                          ('clf', RandomForestRegressor()),
+    ])
+    regressor.fit(train_data, train_labels)
+
+    data = list(read_json(sys.stdin, 2000))
+    predicted = regressor.predict(data)
+    for i in range(len(predicted)):
+        # Take the color for this group of predictions
+        c = cmp(predicted[i], 0)
+        print("{}{:.2f}{}\t{}".format(analyzer.colors[c] if c != 0 else "", predicted[i], analyzer.END_COLOR, data[i].replace('\n',' ')))
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
