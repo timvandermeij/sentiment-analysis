92530bff0eaff269236e2a8d3e333e91c971b604
Tim van der Meij <timvandermeij@gmail.com>
2015-03-11 16:11:47 +0100

    Refactor preprocess.py to a class

--- preprocess.py
+++ preprocess.py
@@ -5,74 +5,79 @@ import tarfile
 import bson
 import json
 
-DATASET_NAME = "commit_comments"
-DATASET_URL = "http://ghtorrent.org/downloads/" + DATASET_NAME + "-dump.2015-01-29.tar.gz"
-DATASET_KEEP_FIELDS = ["id", "body"]
+class Preprocessor(object):
+    def __init__(self):
+        self.keep_fields = ['id', 'body']
 
-def download(url, target):
-    stream = urllib2.urlopen(url)
-    file = open(target, 'wb')
-    fileSize = int(stream.info().getheaders("Content-Length")[0])
-    downloadedSize = 0
-    blockSize = 8192
-    
-    while True:
-        buffer = stream.read(blockSize)
-        if not buffer:
-            break
+    def download(self, url, target):
+        stream = urllib2.urlopen(url)
+        file = open(target, 'wb')
+        file_size = int(stream.info().getheaders('Content-Length')[0])
+        downloaded_size = 0
+        block_size = 8192
+        
+        while True:
+            buffer = stream.read(block_size)
+            if not buffer:
+                break
 
-        downloadedSize += len(buffer)
-        file.write(buffer)
-        status = "Downloading dataset [%3.0f%%]" % (downloadedSize * 100. / fileSize)
-        status += chr(8) * (len(status) + 1)
-        print(status),
+            downloaded_size += len(buffer)
+            file.write(buffer)
+            status = 'Downloading dataset [%3.0f%%]' % (downloaded_size * 100. / file_size)
+            status += chr(8) * (len(status) + 1)
+            print(status),
 
-    print("Downloading dataset [finished]")
-    file.close()
+        print('Downloading dataset [finished]')
+        file.close()
 
-def extract(tarball, file):
-    tar = tarfile.open(tarball + ".tar.gz")
-    member = tar.getmember(file)
-    member.name = os.path.basename(member.name)
-    tar.extract(member)
-    tar.close()
-    print("Untarring dataset [finished]")
+    def extract(self, tarball, file):
+        tar = tarfile.open(tarball + '.tar.gz')
+        member = tar.getmember(file)
+        member.name = os.path.basename(member.name)
+        tar.extract(member)
+        tar.close()
+        print('Untarring dataset [finished]')
 
-def isLatinAlphabet(string):
-    try:
-        string.encode('ascii')
-    except UnicodeEncodeError:
-        return False
-    
-    return True
+    def is_latin(self, string):
+        try:
+            string.encode('ascii')
+        except UnicodeEncodeError:
+            return False
+        
+        return True
 
-def bson2json(file):
-    output = open(file + ".json", "wb")
-    bsonFile = open(file + ".bson", "rb")
-    # Read every BSON object as an iterator to save memory.
-    for rawJson in bson.decode_file_iter(bsonFile):
-        # TODO: Perhaps just strip non-Latin and only ignore if nothing is left
-        if not isLatinAlphabet(rawJson["body"]):
-            continue
+    def bson_to_json(self, file):
+        output = open(file + '.json', 'wb')
+        bson_file = open(file + '.bson', 'rb')
+        
+        # Read every BSON object as an iterator to save memory.
+        for raw_json in bson.decode_file_iter(bson_file):
+            if not self.is_latin(raw_json['body']):
+                continue
 
-        preprocessedJson = {}
-        for item in DATASET_KEEP_FIELDS:
-            preprocessedJson[item] = rawJson[item]
-        json.dump(preprocessedJson, output)
-        output.write("\n")
+            preprocessed_json = {}
+            for item in self.keep_fields:
+                preprocessed_json[item] = raw_json[item]
+            json.dump(preprocessed_json, output)
+            output.write('\n')
 
-    output.close()
-    bsonFile.close()
-    print("Converting BSON to JSON and removing unused fields [finished]")
+        output.close()
+        bson_file.close()
+        print('Converting BSON to JSON and removing unused fields [finished]')
 
 def main(argv):
-    if not os.path.isfile(DATASET_NAME + ".tar.gz"):
-        download(DATASET_URL, DATASET_NAME + ".tar.gz")
-    if not os.path.isfile(DATASET_NAME + ".bson"):
-        extract(DATASET_NAME, "dump/github/" + DATASET_NAME + ".bson")
+    dataset_name = 'commit_comments'
+    dataset_url = 'http://ghtorrent.org/downloads/' + dataset_name + '-dump.2015-01-29.tar.gz'
 
-    bson2json(DATASET_NAME)
-    os.remove(DATASET_NAME + ".bson")
+    preprocessor = Preprocessor()
+
+    if not os.path.isfile(dataset_name + '.tar.gz'):
+        preprocessor.download(dataset_url, dataset_name + '.tar.gz')
+    if not os.path.isfile(dataset_name + '.bson'):
+        preprocessor.extract(dataset_name, 'dump/github/' + dataset_name + '.bson')
+
+    preprocessor.bson_to_json(dataset_name)
+    os.remove(dataset_name + '.bson')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
