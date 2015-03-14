18c45e77ad17c4f8ce15f967ae4a75db6b6b6acf
Leon Helwerda <leon.helwerda@planet.nl>
2015-03-14 19:21:57 +0100

    Cleanup to label.py
    - Make the options that one can choose stand out more with color, so now
      the `p` in `[p]ositive` is green, etc. This makes the difference
      between negative and neutral stand out more. The code is flexible in
      finding where to add the color now, and it might even work if we want
      to make the key bindings flexible later.
    - Rewrote most prints and such to use `.format` instead of `+` operator,
      except for trivial `+ '\n'` code. Removed need of at least one `str`.
    - Ask for the label again if an incorrect choice is given, in order to
      prevent a KeyError. Also allow pressing `q` to quit, might be nicer
      than having to do Ctrl-C.

--- label.py
+++ label.py
@@ -2,12 +2,18 @@ import sys
 import os
 import linecache
 import json
+from collections import OrderedDict
 from analyze import Analyzer
 
 class Labeler:
     def __init__(self, dataset):
         self.dataset = dataset
-        self.labels = {'p': 'positive', 'n': 'negative', 't': 'neutral', 'u': 'unknown'}
+        self.labels = OrderedDict([
+            ('p', 'positive'),
+            ('n', 'negative'),
+            ('t', 'neutral'),
+            ('u', 'unknown')
+        ])
         
         if not os.path.isfile(dataset + '.json'):
             print('Dataset file "' + dataset + '" not found')
@@ -20,9 +26,15 @@ class Labeler:
 
     def start(self):
         analyzer = Analyzer('id')
+        options = []
+        for k, v in self.labels.items():
+            opt = '[{}]'.format(analyzer.get_colored_text(v, k))
+            options.append(v.replace(k, opt) if k in v else "{} {}".format(opt, v))
+        choices = ', '.join(options)
+
         while self.labeled_num_lines < self.original_num_lines:
             line = self.labeled_num_lines + 1
-            print(analyzer.get_colored_text('head', '--- Labeling message ' + str(line) + ' ---'))
+            print(analyzer.get_colored_text('head', '--- Labeling message {} ---'.format(line)))
             
             # linecache provides random access to lines in (large) text files
             raw_json = linecache.getline(self.dataset + '.json', line)
@@ -31,10 +43,15 @@ class Labeler:
             label = analyzer.analyze(message)[0]
             
             print(message + '\n')
-            print('Guess: ' + analyzer.get_colored_text(label))
-            choice = raw_input('Label (Enter to confirm, or [p]ositive, [n]egative, neu[t]ral or [u]nknown): ')
+            print('Guess: {}'.format(analyzer.get_colored_text(label)))
+            choice = '?'
+            while choice != '' and choice not in self.labels:
+                choice = raw_input('Label (Enter to confirm, or {}): '.format(choices))
+                if choice == 'q':
+                    return
+
             text = self.labels[choice] if choice is not '' else analyzer.score_to_label(label)
-            print('You entered: ' + analyzer.get_colored_text(text, text) + '\n')
+            print('You entered: {}'.format(analyzer.get_colored_text(text, text)))
 
             self.write(json_object, text)
             self.labeled_num_lines += 1
