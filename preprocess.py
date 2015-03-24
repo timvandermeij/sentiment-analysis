c82bdae940764529b07417a2b0c7ec0883ad0d8b
Tim van der Meij <timvandermeij@gmail.com>
2015-03-24 13:26:11 +0100

    Add TODOs

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,11 @@ import re
 import gzip
 import shelve
 
+# TODO: do not untar completely, but use directly when untarring
+# TODO: cleanup name, url, file and output parameters
+# TODO: cleanup main function (pass in an array of dates)
+# TODO: what to do with remaining repositories without a language?
+# TODO: what to do with 'null' language?
 class Preprocessor(object):
     def __init__(self):
         self.name = ''
