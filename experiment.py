1f75ce8c208bdcefcc2793e9e0906d4cd53394b3
Tim van der Meij <timvandermeij@gmail.com>
2015-04-16 21:55:56 +0200

    Improve experiment runner with parameters
    - Implemented a way to enable or disable certain algorithms, which makes
      it easier to tune the parameters of an algorithm without having to run
      all the others too.
    - Implemented parameters for the algorithms. The algorithms are now
      represented as a list of dictionaries. Each algorithm has a dictionary
      with a friendly name, the class name, a dictionary of parameters and
      whether or not the algorithm is enabled.
    
    If we run this, we see that the passive aggressive classifier and the
    random forest classifier usually perform best.

--- experiment.py
+++ experiment.py
@@ -13,31 +13,86 @@ class Experiment_Classifier(Classifier):
     def __init__(self):
         Classifier.__init__(self, 'id', 100)
 
-    def create_model(self, class_name):
+    def create_model(self, class_name, parameters):
         models = [
             ('tfidf', TfidfVectorizer(input='content', tokenizer=Utilities.split)),
-            ('clf', class_name())
+            ('clf', class_name(**parameters))
         ]
         self.regressor = Pipeline(models)
         self.train()
 
 def main(argv):
-    folds = int(argv[0]) if len(argv) > 0 else ""
-    algorithms = {
-        'Ada Boost classifier': AdaBoostClassifier,
-        'Extra trees classifier': ExtraTreesClassifier,
-        'Random forest classifier': RandomForestClassifier,
-        'Passsive aggressive classifier': PassiveAggressiveClassifier,
-        'SGD classifier': SGDClassifier,
-        'Bernoulli naive Bayes classifier': BernoulliNB,
-        'K neighbors classifier': KNeighborsClassifier
-    }
+    folds = int(argv[0]) if len(argv) > 0 else 5
+    algorithms = [
+        {
+            'name': 'Ada Boost classifier',
+            'class_name': AdaBoostClassifier,
+            'parameters': {
+                'n_estimators': 100,
+                'learning_rate': 0.5
+            },
+            'enabled': True
+        },
+        {
+            'name': 'Extra trees classifier',
+            'class_name': ExtraTreesClassifier,
+            'parameters': {
+                'n_estimators': 100,
+                'n_jobs': 2
+            },
+            'enabled': True
+        },
+        {
+            'name': 'Random forest classifier',
+            'class_name': RandomForestClassifier,
+            'parameters': {
+                'n_estimators': 100,
+                'n_jobs': 2
+            },
+            'enabled': True
+        },
+        {
+            'name': 'Passsive aggressive classifier',
+            'class_name': PassiveAggressiveClassifier,
+            'parameters': {
+                'C': 0.2,
+                'n_jobs': 2
+            },
+            'enabled': True
+        },
+        {
+            'name': 'SGD classifier',
+            'class_name': SGDClassifier,
+            'parameters': {
+                'alpha': 0.001,
+                'n_iter': 10
+            },
+            'enabled': True
+        },
+        {
+            'name': 'Bernoulli naive Bayes classifier',
+            'class_name': BernoulliNB,
+            'parameters': {
+                'alpha': 0.001
+            },
+            'enabled': True
+        },
+        {
+            'name': 'K neighbors classifier',
+            'class_name': KNeighborsClassifier,
+            'parameters': {},
+            'enabled': True
+        }
+    ]
 
-    for algorithm, class_name in algorithms.iteritems():
+    for algorithm in algorithms:
+        if not algorithm['enabled']:
+            continue
+
         classifier = Experiment_Classifier()
-        classifier.create_model(class_name)
+        classifier.create_model(algorithm['class_name'], algorithm['parameters'])
 
-        print(Utilities.get_colored_text(0, '::: {} :::'.format(algorithm)))
+        print(Utilities.get_colored_text(0, '::: {} :::'.format(algorithm['name'])))
         print('Performing cross-validation on {} folds'.format(folds))
         results = classifier.cross_validate(folds)
         print('Folds: {}'.format(results))
