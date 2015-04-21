81bbcfd2465471d9294c63fb2bacac0598720ff4
Leon Helwerda <leon.helwerda@planet.nl>
2015-04-21 19:26:34 +0200

    Convert algorithms list to a JSON manifest file
    - The algorithms, names and parameters are now stored in a JSON file
      within the repository. The class_name entries that had "class types"
      were converted into two entries containing the full module name of the
      class as well as the class name as strings. The following script was
      used:
    
          # Convert to manifest
          for algorithm in algorithms:
              algorithm['module'] = algorithm['class_name'].__module__
              algorithm['class_name'] = algorithm['class_name'].__name__
    
          with open('algorithms.json', 'w') as file:
              json.dump(algorithms, file, indent=4, separators=(',', ': '))
    
      The module and class name are converted back to Python types using the
      standard importlib library and the module's dictionary.
    - Use the separators parameter for the experiment results JSON so that
      the file does not have trailing spaces on lines.
    - As an added bonus, the filter argument can now also filter on the
      module name, so 'svm' can be used to run the SVC classifiers.
      Refactored the condition to make it less long (and another use of list
      comprehensions).
    - Commented code to explain what it does.

--- experiment.py
+++ experiment.py
@@ -8,6 +8,7 @@ from sklearn.svm import *
 import sys
 import os
 import itertools
+import importlib
 import json
 from classify import Classifier
 from utils import Utilities
@@ -15,122 +16,60 @@ from utils import Utilities
 def main(argv):
     folds = int(argv[0]) if len(argv) > 0 else 5
     filter = argv[1].lower() if len(argv) > 1 else ""
-    algorithms = [
-        {
-            'name': 'Ada Boost classifier',
-            'class_name': AdaBoostClassifier,
-            'parameters': {
-                'n_estimators': [10, 50, 100, 250, 500],
-                'learning_rate': [0.1, 0.2, 0.3, 0.4, 0.5]
-            }
-        },
-        {
-            'name': 'Extra trees classifier',
-            'class_name': ExtraTreesClassifier,
-            'parameters': {
-                'n_estimators': [10, 50, 100, 250, 500],
-                'n_jobs': [2]
-            }
-        },
-        {
-            'name': 'Random forest classifier',
-            'class_name': RandomForestClassifier,
-            'parameters': {
-                'n_estimators': [10, 50, 100, 250, 500],
-                'n_jobs': [2]
-            }
-        },
-        {
-            'name': 'Passsive aggressive classifier',
-            'class_name': PassiveAggressiveClassifier,
-            'parameters': {
-                'C': [0.1, 0.2, 0.3, 0.4, 0.5],
-                'n_jobs': [2]
-            }
-        },
-        {
-            'name': 'SGD classifier',
-            'class_name': SGDClassifier,
-            'parameters': {
-                'alpha': [0.0001, 0.001, 0.005, 0.01, 0.2],
-                'n_iter': [2, 4, 6, 8, 10, 15, 20]
-            }
-        },
-        {
-            'name': 'Gaussian naive Bayes classifier',
-            'class_name': GaussianNB,
-            'parameters': {},
-            'dense': True
-        },
-        {
-            'name': 'Multinomial naive Bayes classifier',
-            'class_name': MultinomialNB,
-            'parameters': {
-                'alpha': [0.2, 0.4, 0.5, 0.7, 0.8]
-            },
-            'dense': True
-        },
-        {
-            'name': 'Bernoulli naive Bayes classifier',
-            'class_name': BernoulliNB,
-            'parameters': {
-                'alpha': [0.0001, 0.001, 0.005, 0.01, 0.2]
-            }
-        },
-        {
-            'name': 'K neighbors classifier',
-            'class_name': KNeighborsClassifier,
-            'parameters': {
-                'n_neighbors': [5, 10, 15, 20, 25, 30, 50],
-                'weights': ['uniform', 'distance']
-            }
-        },
-        {
-            'name': 'Linear SVC',
-            'class_name': LinearSVC,
-            'parameters': {
-                'C': [0.1, 0.2, 0.3, 0.4, 0.5]
-            }
-        },
-        {
-            'name': 'Nu SVC',
-            'class_name': NuSVC,
-            'parameters': {
-                'nu': [0.1, 0.2, 0.3, 0.4, 0.5]
-            }
-        }
-    ]
+
+    # Fields to check whether the filter, if given, appears in.
+    filter_fields = ['name', 'class_name', 'module']
+
+    # Read the manifest containing algorithm descriptions.
+    with open('algorithms.json', 'r') as manifest:
+        algorithms = json.load(manifest)
 
     results = {}
     for algorithm in algorithms:
+        # Skip running the algorithm if it is disabled or the filter name does 
+        # not appear in any of the fields.
         if 'disabled' in algorithm and algorithm['disabled']:
             continue
-        if filter and filter not in algorithm['name'].lower() and filter not in algorithm['class_name'].__name__.lower():
+        if filter and all([filter not in algorithm[k].lower() for k in filter_fields]):
             continue
 
+        # Convert manifest entries to classifier class and parameters
+        module = importlib.import_module(algorithm['module'])
+        class_name = module.__dict__[algorithm['class_name']]
         dense = algorithm['dense'] if 'dense' in algorithm else False
+
+        # Create all possible combinations of parameters.
         parameter_combinations = itertools.product(*algorithm['parameters'].values())
         for combination in parameter_combinations:
             classifier = Classifier('id')
-            class_name = algorithm['class_name']
+
+            # Turn the selected parameter combination back into a dictionary
             parameters = dict(zip(algorithm['parameters'].keys(), combination))
+
+            # Create the model according to the parameters
             classifier.create_model(train=False, class_name=class_name, parameters=parameters, dense=dense)
 
+            # Print the classifier and its parameters nicely
             parameter_string = ', '.join("%s=%r" % (key,val) for (key,val) in parameters.iteritems())
             if parameter_string == "":
                 parameter_string = "none"
             print(Utilities.get_colored_text(0, '::: {} ({}) :::'.format(algorithm['name'], parameter_string)))
+
+            # Run cross-validation and print results
             result = classifier.output_cross_validate(folds)
+            print('')
+
+            # Write the result measurements into the results dictionary.
             if algorithm['name'] not in results:
                 results[algorithm['name']] = {}
             results[algorithm['name']][parameter_string] = {
                 'average': result.mean(),
                 'standard_deviation': result.std()
             }
-            print('')
 
+    # Write the results into a pretty-printed JSON file
     with open('experiment_results.json', 'wb') as file:
-        json.dump(results, file, indent=4)
+        json.dump(results, file, indent=4, separators=(',', ': '))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
