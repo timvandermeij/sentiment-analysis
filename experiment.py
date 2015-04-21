74225626e86523d8393ecceb9c34c9cd468b740d
Tim van der Meij <timvandermeij@gmail.com>
2015-04-21 15:12:04 +0200

    Allow combinations of parameters
    Each parameter is a list of values.

--- experiment.py
+++ experiment.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import *
 from sklearn.svm import *
 import sys
 import os
+import itertools
 from classify import Classifier
 from utils import Utilities
 
@@ -18,40 +19,40 @@ def main(argv):
             'name': 'Ada Boost classifier',
             'class_name': AdaBoostClassifier,
             'parameters': {
-                'n_estimators': 100,
-                'learning_rate': 0.5
+                'n_estimators': [10, 50, 100, 250, 500],
+                'learning_rate': [0.1, 0.2, 0.3, 0.4, 0.5]
             }
         },
         {
             'name': 'Extra trees classifier',
             'class_name': ExtraTreesClassifier,
             'parameters': {
-                'n_estimators': 100,
-                'n_jobs': 2
+                'n_estimators': [10, 50, 100, 250, 500],
+                'n_jobs': [2]
             }
         },
         {
             'name': 'Random forest classifier',
             'class_name': RandomForestClassifier,
             'parameters': {
-                'n_estimators': 100,
-                'n_jobs': 2
+                'n_estimators': [10, 50, 100, 250, 500],
+                'n_jobs': [2]
             }
         },
         {
             'name': 'Passsive aggressive classifier',
             'class_name': PassiveAggressiveClassifier,
             'parameters': {
-                'C': 0.2,
-                'n_jobs': 2
+                'C': [0.1, 0.2, 0.3, 0.4, 0.5],
+                'n_jobs': [2]
             }
         },
         {
             'name': 'SGD classifier',
             'class_name': SGDClassifier,
             'parameters': {
-                'alpha': 0.001,
-                'n_iter': 10
+                'alpha': [0.0001, 0.001, 0.005, 0.01, 0.2],
+                'n_iter': [2, 4, 6, 8, 10, 15, 20]
             }
         },
         {
@@ -64,7 +65,7 @@ def main(argv):
             'name': 'Multinomial naive Bayes classifier',
             'class_name': MultinomialNB,
             'parameters': {
-                'alpha': 0.8
+                'alpha': [0.2, 0.4, 0.5, 0.7, 0.8]
             },
             'dense': True
         },
@@ -72,29 +73,29 @@ def main(argv):
             'name': 'Bernoulli naive Bayes classifier',
             'class_name': BernoulliNB,
             'parameters': {
-                'alpha': 0.01
+                'alpha': [0.0001, 0.001, 0.005, 0.01, 0.2]
             }
         },
         {
             'name': 'K neighbors classifier',
             'class_name': KNeighborsClassifier,
             'parameters': {
-                'n_neighbors': 25,
-                'weights': 'distance'
+                'n_neighbors': [5, 10, 15, 20, 25, 30, 50],
+                'weights': ['uniform', 'distance']
             }
         },
         {
             'name': 'Linear SVC',
             'class_name': LinearSVC,
             'parameters': {
-                'C': 0.8
+                'C': [0.1, 0.2, 0.3, 0.4, 0.5]
             }
         },
         {
             'name': 'Nu SVC',
             'class_name': NuSVC,
             'parameters': {
-                'nu': 0.8
+                'nu': [0.1, 0.2, 0.3, 0.4, 0.5]
             }
         }
     ]
@@ -106,12 +107,16 @@ def main(argv):
             continue
 
         dense = algorithm['dense'] if 'dense' in algorithm else False
-        classifier = Classifier('id')
-        classifier.create_model(train=False, class_name=algorithm['class_name'], parameters=algorithm['parameters'], dense=dense)
+        parameter_combinations = itertools.product(*algorithm['parameters'].values())
+        for combination in parameter_combinations:
+            classifier = Classifier('id')
+            class_name = algorithm['class_name']
+            parameters = dict(zip(algorithm['parameters'].keys(), combination))
+            classifier.create_model(train=False, class_name=class_name, parameters=parameters, dense=dense)
 
-        print(Utilities.get_colored_text(0, '::: {} :::'.format(algorithm['name'])))
-        classifier.output_cross_validate(folds)
-        print('')
+            print(Utilities.get_colored_text(0, '::: {} ({}) :::'.format(algorithm['name'], str(parameters))))
+            classifier.output_cross_validate(folds)
+            print('')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
